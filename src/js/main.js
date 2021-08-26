df8dfea21b3bc7fe2aa646d0db47757a5f33c75c
Konstantin Kazlov <80503351+kkazlov@users.noreply.github.com>
2021-08-26 07:47:40 +0300

    html. client section (slider)

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,19 @@
 "use strict";
+import { tns } from "../../node_modules/tiny-slider/src/tiny-slider";
+
 document.addEventListener('DOMContentLoaded', () => {
     console.log('js start');
+
+    tns({
+        container: '.slider__wrapper',
+        items: 1,
+        autoplay: true,
+        nav: false,
+        autoplayButtonOutput: false,
+        speed: 300,
+        autoplayTimeout: 3500,
+        controlsContainer: '.slider__nav',
+        prevButton: '.slider__prev',
+        nextButton: '.slider__next',
+    });
 });
