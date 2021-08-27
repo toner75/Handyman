fa8830e0aed9fadde9501b5b1d553f07f7e791fe
Konstantin Kazlov <80503351+kkazlov@users.noreply.github.com>
2021-08-27 09:04:55 +0300

    html. add contact page

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,24 @@
 "use strict";
 import { tns } from "../../node_modules/tiny-slider/src/tiny-slider";
 
+
 document.addEventListener('DOMContentLoaded', () => {
     console.log('js start');
 
-    tns({
-        container: '.slider__wrapper',
-        items: 1,
-        autoplay: true,
-        nav: false,
-        autoplayButtonOutput: false,
-        speed: 300,
-        autoplayTimeout: 3500,
-        controlsContainer: '.slider__nav',
-        prevButton: '.slider__prev',
-        nextButton: '.slider__next',
-    });
+    try {
+        
+        tns({
+            container: '.slider__wrapper',
+            items: 1,
+            autoplay: true,
+            nav: false,
+            autoplayButtonOutput: false,
+            speed: 300,
+            autoplayTimeout: 3500,
+            controlsContainer: '.slider__nav',
+            prevButton: '.slider__prev',
+            nextButton: '.slider__next',
+        });
+    } catch(err){}
+    
 });
