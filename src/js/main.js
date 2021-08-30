0ae63a3c09b6dfa2b6c00267e4bf08c918de7149
Konstantin Kazlov <80503351+kkazlov@users.noreply.github.com>
2021-08-30 10:32:27 +0300

    html. services refactoring

--- src/js/main.js
+++ src/js/main.js
@@ -1,48 +1,50 @@
 "use strict";
 import { tns } from "../../node_modules/tiny-slider/src/tiny-slider";
 
-
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('js start');
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("js start");
 
     try {
         tns({
-            container: '#slider-clients__wrapper',
+            container: "#slider-clients__wrapper",
             items: 1,
             autoplay: true,
             nav: false,
             autoplayButtonOutput: false,
             speed: 300,
             autoplayTimeout: 3500,
-            controlsContainer: '#slider-clients__nav',
-            prevButton: '#slider-clients__prev',
-            nextButton: '#slider-clients__next',
+            controlsContainer: "#slider-clients__nav",
+            prevButton: "#slider-clients__prev",
+            nextButton: "#slider-clients__next",
         });
+    } catch (err) {}
 
+    try {
         tns({
-            container: '#slider-gallery__wrapper',
+            container: "#slider-gallery__wrapper",
             items: 3,
             autoplay: false,
             nav: false,
             gutter: 30,
             speed: 300,
-            
-            controlsContainer: '#slider-gallery__nav',
-            prevButton: '#slider-gallery__prev',
-            nextButton: '#slider-gallery__next',
-        });
 
+            controlsContainer: "#slider-gallery__nav",
+            prevButton: "#slider-gallery__prev",
+            nextButton: "#slider-gallery__next",
+        });
+    } catch (err) {}
+
+    try {
         tns({
-            container: '#slider-project__wrapper',
+            container: "#slider-project__wrapper",
             items: 1,
             autoplay: false,
             nav: false,
             speed: 300,
-            
-            controlsContainer: '#slider-project__nav',
-            prevButton: '#slider-project__prev',
-            nextButton: '#slider-project__next',
+
+            controlsContainer: "#slider-project__nav",
+            prevButton: "#slider-project__prev",
+            nextButton: "#slider-project__next",
         });
-    } catch(err){}
-    
+    } catch (err) {}
 });
