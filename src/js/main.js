722eea4a86d678f6cb1053ddc9f5ea8b4b3b64c9
Konstantin Kazlov <80503351+kkazlov@users.noreply.github.com>
2021-08-31 09:19:44 +0300

    adapt. service-page

--- src/js/main.js
+++ src/js/main.js
@@ -22,15 +22,28 @@ document.addEventListener("DOMContentLoaded", () => {
     try {
         tns({
             container: "#slider-gallery__wrapper",
-            items: 3,
+            items: 1,
             autoplay: false,
             nav: false,
-            gutter: 30,
+            gutter: 5,
             speed: 300,
+            edgePadding: 60,
 
             controlsContainer: "#slider-gallery__nav",
             prevButton: "#slider-gallery__prev",
             nextButton: "#slider-gallery__next",
+            responsive: {
+                600: {
+                    items: 3,
+                    gutter: 30,
+                },
+
+                450: {
+                    items: 2,
+                    gutter: 30,
+                },
+
+            },
         });
     } catch (err) {}
 
